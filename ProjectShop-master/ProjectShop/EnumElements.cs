ae0574af2507d2952bad8e2698ebc6d3534d2065
Rino Reyns <lukaszpindor1@gmail.com>
2017-05-25 11:31:30 +0200

    Documentation to EnumElements Class.

--- ProjectShop-master/ProjectShop/EnumElements.cs
+++ ProjectShop-master/ProjectShop/EnumElements.cs
@@ -1,10 +1,129 @@
 ﻿namespace ProjectShop
 {
-    public enum Category { Electronics, Fasion, Home, Music }
-    public enum Electronics { Micro }
-    public enum Fasion { Boot, Shoes, Dress }
-    public enum Home { Nails, Table, Door }
-    public enum Music { Cd, Amp, Guitar }
-    public enum Color { Black, Pink, Yellow, Blue, White }
-    public enum ColorBlack { Black }
+    /// <summary>
+    /// Lista kategorii produktów stworzona na potrzeby wersji demo, bez bazy danych.
+    /// </summary>
+    public enum Category
+    {   /// <summary>
+        /// Kategoria: Elektronika
+        /// </summary>
+        Electronics,
+        /// <summary>
+        /// Kategoria: Moda
+        /// </summary>
+        Fasion,
+        /// <summary>
+        /// Kategoria: Dom
+        /// </summary>
+        Home,
+        /// <summary>
+        /// Kategoria : Muzyka
+        /// </summary>
+        Music
+    }
+
+    /// <summary>
+    /// Lista  produktów w kategorii Elektronika stworzona na potrzeby wersji demo, bez bazy danych.
+    /// </summary>
+    public enum Electronics
+    {   /// <summary>
+        /// Produkt: Micro
+        /// </summary>
+        Micro
+    }
+
+    /// <summary>
+    /// Lista  produktów w kategorii Moda stworzona na potrzeby wersji demo, bez bazy danych.
+    /// </summary>
+    public enum Fasion
+    {
+        /// <summary>
+        /// Produkt: Buty
+        /// </summary>
+        Boot,
+        /// <summary>
+        /// Produk: Trampki
+        /// </summary>
+        Shoes,
+        /// <summary>
+        /// Produkt: Sukienka
+        /// </summary>
+        Dress
+    }
+
+    /// <summary>
+    /// Lista  produktów w kategorii Dom stworzona na potrzeby wersji demo, bez bazy danych.
+    /// </summary>
+    public enum Home
+    {   
+        /// <summary>
+        /// Produkt: Gwoździe
+        /// </summary>
+        Nails,
+        /// <summary>
+        /// Produkt: Stół
+        /// </summary>
+        Table,
+        /// <summary>
+        /// Produkt: Drzwi
+        /// </summary>
+        Door
+    }
+
+    /// <summary>
+    /// Lista  produktów w kategorii Muzyka stworzona na potrzeby wersji demo, bez bazy danych.
+    /// </summary>
+    public enum Music
+    {
+        /// <summary>
+        /// Produkt: Płyta CD
+        /// </summary>
+        Cd,
+        /// <summary>
+        /// Produkt: Wzmacniacz
+        /// </summary>
+        Amp,
+        /// <summary>
+        /// Produkt: Gitara
+        /// </summary>
+        Guitar
+    }
+
+    /// <summary>
+    /// Lista  produktów w kategorii Kolor stworzona na potrzeby wersji demo, bez bazy danych.
+    /// </summary>
+    public enum Color
+    {
+        /// <summary>
+        /// Kolor: Czarny
+        /// </summary>
+        Black,
+        /// <summary>
+        /// Kolor: Różowy
+        /// </summary>
+        Pink,
+        /// <summary>
+        /// Kolor: Żółty
+        /// </summary>
+        Yellow,
+        /// <summary>
+        /// Kolor: Niebieski
+        /// </summary>
+        Blue,
+        /// <summary>
+        /// Kolor: Biały
+        /// </summary>
+        White
+    }
+
+    /// <summary>
+    /// Domyślny kolor dla produktów bez opcji innych kolorów stworzony na potrzeby wersji demo, bez bazy danych.
+    /// </summary>
+    public enum ColorBlack
+    {
+        /// <summary>
+        /// Kolor: Czarny
+        /// </summary>
+        Black
+    }
 }
