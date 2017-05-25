bdaab5c920f67ca528b13cae6f3daf8bd33dde0c
Rino Reyns <lukaszpindor1@gmail.com>
2017-05-25 09:00:23 +0200

    ExitWindow documentation

--- ProjectShop-master/ProjectShop/ExitWindow.cs
+++ ProjectShop-master/ProjectShop/ExitWindow.cs
@@ -2,9 +2,21 @@
 
 namespace ProjectShop
 {
+    /// <summary>
+    /// Klasa odpowiedzialna za zamykanięcie aplikacji przy pomocy "krzyżyka" na górnym pasku aplikacji oraz przyciskiem "Koniec". Podczas zamykania
+    /// pokazywane jest również okno interakacji z użytkownikiem. Gdy zamknięcie okna odbywa się przy pomocy przysisku "Koniec", kolejno zamykana
+    ///  jest strona oraz całe okno aplikacji. Algorytm działania funkcji zapobiega podwójnemu potwierdzaniu zamknięcia okna w przypadku interakcji 
+    /// przy użyciu przycisku "Koniec".
+    /// </summary>
     public static class ExitWindow
     {
         private static int OnlyOneExitMessage { get; set; } = 0;
+        /// <summary>
+        /// Funkcja odpowiedzialna za zamykanie aplikacji zgodnie z opisem umieszczonym w opisie klasy.
+        /// </summary>
+        /// <param name="i">Wartość zmiennej zapobiega podwójnemu zapytaniu użytkownika o zakmnięcie okna w przypadku interakcji z aplikacją 
+        /// za pomocą przycisku "Koniec". Zmienna domyślnie ma wartość 0, lecz gdy użytkownik potwierdzi zamkniecie aplikacji zmienna przyjmuje wartość 1.</param>
+        /// <returns> Funkcja zwraca wartość boolowską, True lub False, w zalezności od tego czy użytkownik potwierdzi zamknięcie aplikacji.</returns>
         public static bool Exit(int i = 0)
         {
             if (OnlyOneExitMessage == 0)
