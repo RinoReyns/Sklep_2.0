c1ec91ea0b5ab8708508b275bd3ab9693dad2712
Rino Reyns <lukaszpindor1@gmail.com>
2017-05-26 20:21:37 +0200

    Adding units tests

---
+++ ProjectShop-master/ProjectShopTests/ControlTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectShop;
+
+namespace ProjectShop.Tests
+{
+    [TestClass()]
+    public class ControlTests
+    {
+        [TestMethod()]
+        public void CheckIfIsOnTheList()
+        {
+            Control control = new Control();
+            var productList = new List<Product> { new Boot(), new Cd(), new Amp() };
+            var state = control.Check(productList, "Amp");
+            Assert.AreEqual(1, state);
+        }
+        [TestMethod()]
+        public void CheckIfIsNotOnTheList()
+        {
+            Control control = new Control();
+            var productList = new List<Product> { new Boot(), new Cd(), new Amp() };
+            var state = control.Check(productList, "Table");
+            Assert.AreEqual(0, state);
+        }
+
+
+    }
+}
+
