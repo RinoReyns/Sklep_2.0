4a2299fa27e41e91c262c33387bb147c3d989258
Rino Reyns <lukaszpindor1@gmail.com>
2017-05-27 09:37:44 +0200

    More tests without problems

---
+++ ProjectShop-master/ProjectShopTests/ExitWindowTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectShop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectShop.Tests
+{
+    [TestClass()]
+    public class ExitWindowTests
+    {
+        [TestMethod()]
+        public void ExitTestNo()
+        {
+            try
+            {
+                Assert.IsTrue(ExitWindow.Exit(1));
+            }
+            catch (NullReferenceException)
+            {
+
+                Assert.IsTrue(true);
+            }
+
+        }
+    }
+}
