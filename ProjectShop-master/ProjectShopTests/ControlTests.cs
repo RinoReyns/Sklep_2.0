c45664a122def4cff57856ab10b5f11afc8096c1
Rino Reyns <lukaszpindor1@gmail.com>
2017-05-27 08:34:43 +0200

    Unit Tests for Control class

--- ProjectShop-master/ProjectShopTests/ControlTests.cs
+++ ProjectShop-master/ProjectShopTests/ControlTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProjectShop;
 
@@ -8,23 +9,55 @@ namespace ProjectShop.Tests
     public class ControlTests
     {
         [TestMethod()]
-        public void CheckIfIsOnTheList()
+        public void CheckIfIsOnTheListByName()
         {
-            Control control = new Control();
-            var productList = new List<Product> { new Boot(), new Cd(), new Amp() };
-            var state = control.Check(productList, "Amp");
-            Assert.AreEqual(1, state);
+            var control = new Control();
+            Assert.AreEqual(1, control.Check(new List<Product> { new Boot(), new Cd(), new Amp() }, "Amp"));
         }
         [TestMethod()]
-        public void CheckIfIsNotOnTheList()
+        public void CheckIfIsNotOnTheListByName()
         {
-            Control control = new Control();
-            var productList = new List<Product> { new Boot(), new Cd(), new Amp() };
-            var state = control.Check(productList, "Table");
-            Assert.AreEqual(0, state);
+            var control = new Control();
+            Assert.AreEqual(0, control.Check(new List<Product> { new Boot(), new Nails(), new Amp() }, "Table"));
+        }
+        [TestMethod()]
+        public void CheckIfIsOnTheListByMoreParameters()
+        {
+            var control = new Control();
+            var testProduct = new Nails
+            {
+                Color = "Pink",
+                SizeItem = "1mm"
+            };
+            Assert.AreNotEqual(-1, control.Check(new ObservableCollection<Product>() { new Micro(), new Nails(), new Door(), testProduct },
+                                                "Nails", "Pink", "1mm"));
+        }
+        [TestMethod()]
+        public void CheckIfIsNotOnTheListByMoreParameters()
+        {
+            var control = new Control();
+            var testProduct = new Nails
+            {
+                Color = "Blue",
+                SizeItem = "1mm"
+            };
+            Assert.AreEqual(-1, control.Check(new ObservableCollection<Product>() { new Guitar(), new Nails(), new Shoes(), testProduct },
+                                                "Nails", "Pink", "1mm"));
         }
 
+        [TestMethod()]
+        public void FinalPriceTestIfEqual()
+        {
+            var control = new Control();
+            Assert.AreEqual("51",control.FinalPrice(new ObservableCollection<Product>() { new Micro(), new Nails(), new Shoes()}));
+        }
 
+        [TestMethod()]
+        public void FinalPriceTestIfNotEqual()
+        {
+            var control = new Control();
+            Assert.AreNotEqual("54", control.FinalPrice(new ObservableCollection<Product>() { new Table(), new Dress(), new Shoes() }));
+        }
     }
 }
 
